0231e0a8d1aafdee9f3619d29927b4613a0b26bd
Thiago Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2025-01-29 06:43:33 -0300

    remove calculadora.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,6 @@
 function status(request, response) {
   response.status(200).json({
-    "message": "Server is up and running!"
+    "message": "Server is up and running! são"
   })
 }
 export default status

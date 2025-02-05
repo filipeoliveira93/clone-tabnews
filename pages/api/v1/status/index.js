28e45a96f26faf3ce5f8e321d0c4d1c3b7ede358
Thiago Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2025-02-05 07:03:32 -0300

    create .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,10 @@
-function status(request, response) {
-  response.status(200).json({
-    "message": "Server is up and running!"
-  })
-}
-export default status
+import database from '../../../../infra/database.js';
 
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1 as sum;');
+  console.log(result.rows);
+  response.status(200).json({
+    message: 'Server is up and running!',
+  });
+}
+export default status;
